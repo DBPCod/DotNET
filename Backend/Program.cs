8fc807fc6dbdc20e8532646c1c1383797cd5989d
LuuD04N <doanphongluu082@gmail.com>
2025-10-21 21:19:49 +0700

    Thêm Swagger để test thôi

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -6,12 +6,20 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 ConfigureExtensions.ConfigureAllBuilder(builder);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 WebApplication app = builder.Build();
 
 // Middleware
 app.UseCors(Variable.Constants.MyAllowSpecificOrigins);
 app.UseAuthentication();
 app.UseAuthorization();
+app.UseSwagger();
+app.UseSwaggerUI(c =>
+{
+    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Backend API v1");
+});
 
 // Controllers
 app.MapControllers();
